1765f1949d4b5add58b337f7125b97e85587fb9a
Svetoslav <venom008@gmail.com>
2026-01-28 17:46:19 +0300

    Improve SNTP

--- main/app_time.c
+++ main/app_time.c
@@ -7,16 +7,32 @@
 #define TAG "APP_TIME"
 
 void setup_time(void) {
-    ESP_LOGI(TAG, "Initializing SNTP with time.google.com");
-    esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG("time.google.com");
+    ESP_LOGI(TAG, "Initializing SNTP");
+
+    esp_sntp_config_t config = {
+        .smooth_sync = false,
+        .server_from_dhcp = false,
+        .wait_for_sync = true,
+        .start = true,
+        .sync_cb = NULL,
+        .renew_servers_after_new_IP = false,
+        .ip_event_to_renew = (ip_event_t)0,
+        .index_of_first_server = 0,
+        .num_of_servers = CONFIG_LWIP_SNTP_MAX_SERVERS
+    };
+    config.servers[0] = "time.cloudflare.com";
+    if (CONFIG_LWIP_SNTP_MAX_SERVERS > 1) {
+        config.servers[1] = "time.google.com";
+    }
     esp_netif_sntp_init(&config);
+
     ESP_LOGI(TAG, "SNTP initialized");
 }
 
 void update_time(void) {
     ESP_LOGI(TAG, "Waiting for NTP time synchronization...");
 
-    esp_err_t ret = esp_netif_sntp_sync_wait(pdMS_TO_TICKS(10000));
+    esp_err_t ret = esp_netif_sntp_sync_wait(pdMS_TO_TICKS(30 * 1000));
     if (ret == ESP_OK) {
         ESP_LOGI(TAG, "Time synchronized successfully!");
         time_t now;
